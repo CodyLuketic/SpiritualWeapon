437ffa75f96172ee7199e091b45de02a6c748a7c
Cody Luketic <luketica@miamioh.edu>
2023-01-30 15:55:14 -0500

    UI Work 1

---
+++ SpiritualWeapon/Assets/DecadePrayersFill.cs
@@ -0,0 +1,235 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecadePrayersFill : MonoBehaviour
+{
+    [SerializeField]
+    private Image outline = null;
+    [SerializeField]
+    private Image lLargeBead = null;
+    [SerializeField]
+    private Image smallBead1 = null;
+    [SerializeField]
+    private Image smallBead2 = null;
+    [SerializeField]
+    private Image smallBead3 = null;
+    [SerializeField]
+    private Image smallBead4 = null;
+    [SerializeField]
+    private Image smallBead5 = null;
+    [SerializeField]
+    private Image smallBead6 = null;
+    [SerializeField]
+    private Image smallBead7 = null;
+    [SerializeField]
+    private Image smallBead8 = null;
+    [SerializeField]
+    private Image smallBead9 = null;
+    [SerializeField]
+    private Image smallBead10 = null;
+    [SerializeField]
+    private Image rLargeBead = null;
+
+    [SerializeField]
+    private Color crossColor;
+    [SerializeField]
+    private Color outlineColor;
+    [SerializeField]
+    private Color beadColor;
+
+    [SerializeField]
+    private float crossTime = 0.1f;
+    [SerializeField]
+    private float largeBeadTime = 0.1f;
+
+    [SerializeField]
+    private float smallBeadTime = 0.1f;
+    [SerializeField]
+    private float increment = 0.1f;
+
+    //private float r;
+    //private float g;
+    //private float b;
+    private float a;
+
+    private void Start()
+    {
+        outline.color = outlineColor;
+        StartCoroutine(Fill());
+    }
+
+    private IEnumerator Fill() {
+        //r = lLargeBead.color.r;
+        //g = lLargeBead.color.g;
+        //b = lLargeBead.color.b;
+        a = lLargeBead.color.a;
+
+        while(lLargeBead.color.a < 1) {
+            ColorChange(lLargeBead, beadColor);
+            yield return new WaitForSeconds(largeBeadTime);
+        }
+
+        Debug.Log("Completed lLargeBead");
+
+        //r = smallBead1.color.r;
+        //g = smallBead1.color.g;
+        //b = smallBead1.color.b;
+        a = smallBead1.color.a;
+
+        while(smallBead1.color.a < 1) {
+            ColorChange(smallBead1, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead1");
+
+        //r = smallBead2.color.r;
+        //g = smallBead2.color.g;
+        //b = smallBead2.color.b;
+        a = smallBead2.color.a;
+
+        while(smallBead2.color.a < 1) {
+            ColorChange(smallBead2, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead2");
+
+        //r = smallbead3.color.r;
+        //g = smallbead3.color.g;
+        //b = smallbead3.color.b;
+        a = smallBead3.color.a;
+
+        while(smallBead3.color.a < 1) {
+            ColorChange(smallBead3, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead3");
+
+        //r = smallbead4.color.r;
+        //g = smallbead4.color.g;
+        //b = smallbead4.color.b;
+        a = smallBead4.color.a;
+
+        while(smallBead4.color.a < 1) {
+            ColorChange(smallBead4, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead4");
+
+        //r = smallbead5.color.r;
+        //g = smallbead5.color.g;
+        //b = smallbead5.color.b;
+        a = smallBead5.color.a;
+
+        while(smallBead5.color.a < 1) {
+            ColorChange(smallBead5, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead5");
+
+        //r = smallbead6.color.r;
+        //g = smallbead6.color.g;
+        //b = smallbead6.color.b;
+        a = smallBead6.color.a;
+
+        while(smallBead6.color.a < 1) {
+            ColorChange(smallBead6, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead6");
+
+        //r = smallbead7.color.r;
+        //g = smallbead7.color.g;
+        //b = smallbead7.color.b;
+        a = smallBead7.color.a;
+
+        while(smallBead7.color.a < 1) {
+            ColorChange(smallBead7, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead7");
+
+        //r = smallbead8.color.r;
+        //g = smallbead8.color.g;
+        //b = smallbead8.color.b;
+        a = smallBead8.color.a;
+
+        while(smallBead8.color.a < 1) {
+            ColorChange(smallBead8, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead8");
+
+        //r = smallbead9.color.r;
+        //g = smallbead9.color.g;
+        //b = smallbead9.color.b;
+        a = smallBead9.color.a;
+
+        while(smallBead9.color.a < 1) {
+            ColorChange(smallBead9, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead9");
+
+        //r = smallbead10.color.r;
+        //g = smallbead10.color.g;
+        //b = smallbead10.color.b;
+        a = smallBead10.color.a;
+
+        while(smallBead10.color.a < 1) {
+            ColorChange(smallBead10, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed smallBead10");
+
+        //r = rLargeBead.color.r;
+        //g = rLargeBead.color.g;
+        //b = rLargeBead.color.b;
+        a = rLargeBead.color.a;
+
+        while(rLargeBead.color.a < 1) {
+            ColorChange(rLargeBead, beadColor);
+            yield return new WaitForSeconds(largeBeadTime);
+        }
+
+        Debug.Log("Completed rLargeBead");
+    }
+
+    private void ColorChange(Image img, Color col) {
+        /*
+        if(r < col.r) {
+            r += increment;
+        } else if(r > col.r) {
+            r -= increment;
+        }
+
+        if(g < col.g) {
+            g += increment;
+        } else if(g > col.g) {
+            g -= increment;
+        }
+
+        if(b < col.b) {
+            b += increment;
+        } else if(b > col.b) {
+            b -= increment;
+        }
+        */
+
+        if(a < 1) {
+            a += increment;
+        }
+
+        img.color = new Color(col.r, col.g, col.b, a);
+    }
+}
---
+++ SpiritualWeapon/Assets/StartingPrayerFill.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingPrayerFill : MonoBehaviour
+{
+    [SerializeField]
+    private Image cross = null;
+    [SerializeField]
+    private Image outline = null;
+    [SerializeField]
+    private Image lLargeBead = null;
+    [SerializeField]
+    private Image lSmallBead = null;
+    [SerializeField]
+    private Image mSmallBead = null;
+    [SerializeField]
+    private Image rSmallBead = null;
+    [SerializeField]
+    private Image rLargeBead = null;
+
+    [SerializeField]
+    private Color crossColor;
+    [SerializeField]
+    private Color outlineColor;
+    [SerializeField]
+    private Color beadColor;
+
+    [SerializeField]
+    private float crossTime = 0.1f;
+    [SerializeField]
+    private float largeBeadTime = 0.1f;
+
+    [SerializeField]
+    private float smallBeadTime = 0.1f;
+    [SerializeField]
+    private float increment = 0.1f;
+
+    //private float r;
+    //private float g;
+    //private float b;
+    private float a;
+
+    private void Start()
+    {
+        outline.color = outlineColor;
+        StartCoroutine(Fill());
+    }
+
+    private IEnumerator Fill() {
+        //r = cross.color.r;
+        //g = cross.color.g;
+        //b = cross.color.b;
+        a = cross.color.a;
+
+        while(cross.color.a < 1) {
+            ColorChange(cross, crossColor);
+            yield return new WaitForSeconds(crossTime);
+        }
+
+        Debug.Log("Completed cross");
+
+        //r = lLargeBead.color.r;
+        //g = lLargeBead.color.g;
+        //b = lLargeBead.color.b;
+        a = lLargeBead.color.a;
+
+        while(lLargeBead.color.a < 1) {
+            ColorChange(lLargeBead, beadColor);
+            yield return new WaitForSeconds(largeBeadTime);
+        }
+
+        Debug.Log("Completed lLargeBead");
+
+        //r = lSmallBead.color.r;
+        //g = lSmallBead.color.g;
+        //b = lSmallBead.color.b;
+        a = lSmallBead.color.a;
+
+        while(lSmallBead.color.a < 1) {
+            ColorChange(lSmallBead, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed lSmallBead");
+
+        //r = mSmallBead.color.r;
+        //g = mSmallBead.color.g;
+        //b = mSmallBead.color.b;
+        a = mSmallBead.color.a;
+
+        while(mSmallBead.color.a < 1) {
+            ColorChange(mSmallBead, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed mSmallBead");
+
+        //r = rSmallBead.color.r;
+        //g = rSmallBead.color.g;
+        //b = rSmallBead.color.b;
+        a = rSmallBead.color.a;
+
+        while(rSmallBead.color.a < 1) {
+            ColorChange(rSmallBead, beadColor);
+            yield return new WaitForSeconds(smallBeadTime);
+        }
+
+        Debug.Log("Completed rSmallBead");
+
+        //r = rLargeBead.color.r;
+        //g = rLargeBead.color.g;
+        //b = rLargeBead.color.b;
+        a = rLargeBead.color.a;
+
+        while(rLargeBead.color.a < 1) {
+            ColorChange(rLargeBead, beadColor);
+            yield return new WaitForSeconds(largeBeadTime);
+        }
+
+        Debug.Log("Completed rLargeBead");
+    }
+
+    private void ColorChange(Image img, Color col) {
+        /*
+        if(r < col.r) {
+            r += increment;
+        } else if(r > col.r) {
+            r -= increment;
+        }
+
+        if(g < col.g) {
+            g += increment;
+        } else if(g > col.g) {
+            g -= increment;
+        }
+
+        if(b < col.b) {
+            b += increment;
+        } else if(b > col.b) {
+            b -= increment;
+        }
+        */
+
+        if(a < 1) {
+            a += increment;
+        }
+
+        img.color = new Color(col.r, col.g, col.b, a);
+    }
+}
