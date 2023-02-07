9beb3d326e1b6d3efb529a86c7ccd904bc85d255
Cody Luketic <luketica@miamioh.edu>
2023-02-06 23:22:51 -0500

    Added Enemies

--- SpiritualWeapon/Assets/Enemies/Scripts/EnemyCombat.cs
+++ SpiritualWeapon/Assets/Enemies/Scripts/EnemyCombat.cs
@@ -3,19 +3,16 @@ using UnityEngine;
 
 public class EnemyCombat : MonoBehaviour
 {
+    
+    [SerializeField] private Animator animator = null;
+
     [SerializeField]
     private float shrinkSpeed = 0.1f, deincrement = 0.01f;
-
-    private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.CompareTag("Player")) {
-            Debug.Log("Collided with Player");
-            StartCoroutine("Shrinker");
-        }
-    }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")) {
             Debug.Log("Collided with Player");
+            animator.SetTrigger("Attack");
             StartCoroutine("Shrinker");
         }
     }
@@ -25,9 +22,14 @@ public class EnemyCombat : MonoBehaviour
             gameObject.transform.localScale += new Vector3(-deincrement, -deincrement, -deincrement);
             yield return new WaitForSeconds(shrinkSpeed);
         }
-        gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<SkinnedMeshRenderer>().enabled = false;
+        gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = false;
+
         gameObject.transform.localScale = new Vector3(1, 1, 1);
-        gameObject.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<SkinnedMeshRenderer>().enabled = true;
+        gameObject.transform.GetChild(0).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = true;
+
         gameObject.SetActive(false);
     }
 }
